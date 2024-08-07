b9fe79609d5439167e5d5c9110c2378b2cbc8de2
AnaAnjos00 <ana.silva88@estudante.ifms.edu.br>
2024-08-07 15:59:44 -0400

    update
    aaaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,34 @@
 import { Link } from "react-router-dom";
 import "./globals.css";
-import Home from "./1-AtividadeCriarComponente/index";
 
 export default function App() {
     return (
-        <div>
-            <h1>Lista de Exercícios</h1>
-            <ol type='1'>
-                <li>
-                    <Link to="/primeiraAtividade">Atividade 1: Criar componente de página Home</Link>
-                </li>
-                <li>
-                    <Link to="/segundaAtividade">Atividade 2: Criar estado para lista de produtos</Link>
-                </li>
-                <li>
-                    <Link to="/terceiraAtividade">Atividade 3: Listar os objetos da lista de produtos</Link>
-                </li>
-                <li>
-                    <Link to="/quartaAtividade">Atividade 4: Criar estado e função para adicionar o objeto na lista de pedidos</Link>
-                </li>
-                <li>
-                    <Link to="/quintaAtividade">Atividade 5: Criar botão para adicionar o objeto na lista de pedidos</Link>
-                </li>
-                <li>
-                    <Link to="/sextaAtividade">Atividade 6: Listar os objetos da lista de pedidos</Link>
-                </li>
-                <li>
-                    <Link to="/setimaAtividade">Atividade 7: Criar a função para remover objeto da lista de pedidos</Link>
-                </li>
-                <li>
-                    <Link to="/oitavaAtividade">Atividade 8: Criar botão para remover o objeto do pedido</Link>
-                </li>
-            </ol>
-        </div>
+        
+            const adicionarProdutoPedido = (produto) => {
+             setPedidos([...listaPedidos, produto]);
+}
+        console.table(listaPedidos);
+
+            const removerItem = (id) => {
+                let remover = false;
+                let listaAux = listaPedidos.filter((pedido)
+                    {
+                        if(pedido.id == id){
+
+                        }
+                    });
+                    setPedidos(listaAux);
+                }           
+
+
+                  //Atividade 2: Criar estado para lista de produtos
+                  //Atividade 3: Listar os objetos da lista de produtos
+                  //Atividade 4: Criar estado e função para adicionar o objeto na lista de pedidos
+                 //Atividade 5: Criar botão para adicionar o objeto na lista de pedidos
+                  //Atividade 6: Listar os objetos da lista de pedidos
+                  //Atividade 7: Criar a função para remover objeto da lista de pedidos
+                 //Atividade 8: Criar botão para remover o objeto do pedido
+               
+        
     );
 }
